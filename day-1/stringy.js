5b4cf5d7d9508bc767a155d9ef2e1429c453fcd1
Gmcjr <gmcostajr@gmail.com>
2026-01-08 05:20:06 -0600

    describe work

--- day-1/stringy.js
+++ day-1/stringy.js
@@ -75,7 +75,9 @@ return string.toUpperCase();
  */
 function toDashCase(string) {
   // YOUR CODE BELOW HERE //
-
+  // find spaces in input string and convert to dashes
+  // enforce lowercase
+  return string.toLowerCase().replace(/\s+/g, '-');
   // YOUR CODE ABOVE HERE //
 }
 
@@ -97,7 +99,12 @@ function toDashCase(string) {
  */
 function beginsWith(string, char) {
   // YOUR CODE BELOW HERE //
-
+// enforce lowercase
+// compare first character of input string with input character
+// return true if they are the same, otherwise return false
+var string = string.toLowerCase();
+var char = char.toLowerCase();
+return string.startsWith(char);
   // YOUR CODE ABOVE HERE //
 }
 
@@ -112,10 +119,20 @@ function beginsWith(string, char) {
  *
  * TIP: How can you use Array access to your advantage here? How can you
  *      ensure uppercase and lowercase can be compared equally?
+ * 
+ * I: function takes a string and a single character as inputs
+ * O: function returns true if input string ends with input single character, otherwise returns false
+ * C: function is case insensitive
+ * E:
  */
 function endsWith(string, char) {
   // YOUR CODE BELOW HERE //
-
+// enforce lowercase
+// compare last character of input string with input character
+// return true if they match, otherwise return false
+var string = string.toLowerCase();
+var char = char.toLowerCase();
+return string.endsWith(char);
   // YOUR CODE ABOVE HERE //
 }
 
@@ -123,10 +140,15 @@ function endsWith(string, char) {
  * Given two input Strings, return the Strings concatenated into one.
  *
  * TIP: What's the operator to concatenate two Strings?
+ * 
+ * I: function takes two strings as inputs
+ * O: function returns the two input strings concatenated into one string
+ * C:
+ * E:
  */
 function concat(stringOne, stringTwo) {
   // YOUR CODE BELOW HERE //
-
+return stringOne + stringTwo;
   // YOUR CODE ABOVE HERE //
 }
 
@@ -139,11 +161,17 @@ function concat(stringOne, stringTwo) {
  *
  * TIP: This Function pulls out all the arguments passed to it and stores them
  *      in an Array called args.
+ * 
+ * I: function takes any number of strings as inputs
+ * O: function returns all input strings joined together
+ * C: function pulls out all arguments passed and stores them in an array
+ * E:
  */
 function join(stringOne, stringTwo) {
   // YOUR CODE BELOW HERE //
   var args = Array.from(arguments);
-
+// join the elements of array args and return the result
+return args.join("");
   // YOUR CODE ABOVE HERE //
 }
 
@@ -155,10 +183,21 @@ function join(stringOne, stringTwo) {
  *      longest("ben", "maggie");   //-> "maggie"
  *
  * TIP: What property of the String do we need to compare?
+ * 
+ * I: function takes two strings as inputs
+ * O: function returns the longest of the two input strings
+ * C:
+ * E:
  */
 function longest(stringOne, stringTwo) {
   // YOUR CODE BELOW HERE //
-
+  // compare lengths of both input strings
+  // return the longeset of the two
+if (stringOne.length > stringTwo.length) {
+  return stringOne;
+  } else {
+    return stringTwo;
+  }
   // YOUR CODE ABOVE HERE //
 }
 
@@ -168,10 +207,19 @@ function longest(stringOne, stringTwo) {
  * first, and return 0 if they're equal.
  *
  * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ * 
+ * I: function takes two strings as inputs
+ * O: function returns 1 if first string is higher in alphabetical order (earlier in alphabet) than string two
+ *  returns -1 if the second is higher in alphabetical order than the first
+ *  returns 0 if the input strings are equal
+ * C:
+ * E:
  */
 function sortAscending(stringOne, stringTwo) {
   // YOUR CODE BELOW HERE //
-
+// compare characters of input strings to determine their alphabetical order
+// return 1 if stringOne > stringTwo, return -1 if stringTwo > stringOne, return 0 if stringOne === stringTwo
+return stringTwo.localeCompare(stringOne);
   // YOUR CODE ABOVE HERE //
 }
 
@@ -181,10 +229,19 @@ function sortAscending(stringOne, stringTwo) {
  * first, and return 0 if they're equal.
  *
  * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ * 
+ * I: function takes two strings as inputs
+ * O: function returns 1 if first string is lower in alphabetical order (later in alphabet) than string two
+ *  returns -1 if the second is lower in alphabetical order than the first
+ *  returns 0 if the input strings are equal
+ * C:
+ * E:
  */
 function sortDescending(stringOne, stringTwo) {
   // YOUR CODE BELOW HERE //
-  
+  // compare characters of input strings to determine their alphabetical order
+// return 1 if stringTwo > stringOne, return -1 if stringOne > stringTwo, return 0 if stringOne === stringTwo
+return stringOne.localeCompare(stringTwo);
   // YOUR CODE ABOVE HERE //
 }
 
--- day-2/loops.js
+++ day-2/loops.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+const { values } = require("lodash");
+
 /**
  * IN CLASS EXERCISE: LOOPS
  */
@@ -9,77 +11,143 @@
 /**
  * Given an input Array, loop forward over the Array and print its values
  * using console.log().
+ * 
+ * I: function takes an array as input
+ * O: function returns values of input array
+ * C: function loops forward over array and uses console.log() to print output
+ * E:
  */
 function printArrayValues(array) {
   // YOUR CODE BELOW HERE //
 
+  // loop forward over input array
+for (var i = 0; i < array.length; i++) {
+  // output print array value to console
+  console.log(array[i]);
+}
   // YOUR CODE ABOVE HERE //
 }
 
 /**
  * Given an input Array, loop backwards over the Array and print its values
  * using console.log().
+ * 
+ * I: function takes an array as input
+ * O: function returns values of input array
+ * C: function loops backward over the array and uses console.log() to print output
+ * E:
  */
 function printArrayValuesInReverse(array) {
   // YOUR CODE BELOW HERE //
-
+  // loop backward over input array
+for (var i = array.length -1; i >= 0; i--) {
+  // output print array value to console
+  console.log(array[i]);
+}
   // YOUR CODE ABOVE HERE //
 }
 
 /**
  * Given an input Object, return an Array containing the Object keys.
+ * 
+ * I: function takes an object as input
+ * O: function returns an array containing the input object keys
+ * C: function returns an array
+ * E:
  */
 function getObjectKeys(object) {
   // YOUR CODE BELOW HERE //
-
+// get object keys and return them in an array
+return Object.keys(object);
   // YOUR CODE ABOVE HERE //
 }
 
 /**
  * Given an input Object, loop over the Object and print its keys
  * using console.log().
+ * 
+ * I: function takes an object as input
+ * O: function prints input object keys
+ * C: function uses console.log() to print keys
+ * E:
  */
 function printObjectKeys(object) {
   // YOUR CODE BELOW HERE //
 
+  // use for in loop to get object keys and print them
+for (var key in object) {
+  console.log(key);
+}
   // YOUR CODE ABOVE HERE //
 }
 
 /**
  * Given an input Object, return an Array containing the Object's values.
+ * 
+ * I: function takes an object as input
+ * O: function returns an array containing the input object key values
+ * C: function returns an array
+ * E:
  */
 function getObjectValues(object) {
   // YOUR CODE BELOW HERE //
-
+// get object values and return them with an array
+  return Object.values(object);
   // YOUR CODE ABOVE HERE //
 }
 
 /**
  * Given an input Object, loop over the Object and print its values
  * using console.log().
+ * 
+ * I: function takes an object as input
+ * O: function prints object values
+ * C: function loops over object and uses console.log() to print keys
+ * E:
  */
 function printObjectValues(object) {
   // YOUR CODE BELOW HERE //
-
+  // loop over input object
+  for (var key in object) {
+  // output values printed to console
+    console.log(object[key]);
+  }
   // YOUR CODE ABOVE HERE //
 }
 
 /**
  * Given an input Object, return the number of key/value pairs stored within that Object.
+ * 
+ * I: function takes an object as input
+ * O: function returns the number of key/value pairs stored within that object
+ * C:
+ * E:
  */
 function getObjectLength(object) {
   // YOUR CODE BELOW HERE //
-
+// get the number of key/value pairs inside the input object
+// return that number
+var objLength = Object.entries(object).length;
+return objLength;
   // YOUR CODE ABOVE HERE //
 }
 
 /**
  * Given an input Object, how might we loop over the Object IN REVERSE and
  * print its values using console.log()?
+ * 
+ * I: function takes an object as input
+ * O: function prints object values in reverse as output
+ * C:
+ * E:
  */
 function printObjectValuesInReverse(object) {
   // YOUR CODE BELOW HERE //
-  
+  // loop over input object
+  for (var key in object) {
+    // output values printed to console
+      console.log(object.reverse([key]));
+    }
   // YOUR CODE ABOVE HERE //
 }
 
